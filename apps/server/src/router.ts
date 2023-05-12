4c490d9ca5057ebe47cc66bb0c1cd616f466cd0e
AgnesGrace <agnesaugustine42@gmail.com>
2023-05-12 13:35:14 +0200

    feat:[MVC]: worked on all the product api routes

--- apps/server/src/router.ts
+++ apps/server/src/router.ts
@@ -2,6 +2,11 @@ import express from 'express'
 const router = express.Router()
 import ProductController from './controllers/products.controller'
 
+// product routes
 router.post('/product', ProductController.createProduct)
+router.get('/product', ProductController.getAllProducts)
+router.get('product/:id', ProductController.getProductById)
+router.put('/product/:productId', ProductController.updateProduct)
+router.delete('/product/:productId', ProductController.deleteProduct)
 
 export default router
