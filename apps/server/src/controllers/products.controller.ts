a2ab2e651c2f453cb7bc02ff6d23e5434e433793
iliasFs <110726288+iliasFs@users.noreply.github.com>
2023-05-13 14:27:56 +0300

    feat:[MVC]:Added and tested all routes working

--- apps/server/src/controllers/products.controller.ts
+++ apps/server/src/controllers/products.controller.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from 'express'
-import Product from '../models/Products'
+import { Request, Response, NextFunction } from "express";
+import Product from "../models/Products";
 // import { S3 } from 'aws-sdk'
 const ProductController = {
   async createProduct(req: Request, res: Response, next: NextFunction) {
@@ -14,7 +14,7 @@ const ProductController = {
         featured,
         newArrivals,
         category,
-      } = req.body
+      } = req.body;
       //trying to handle images with s3
       // const s3 = new S3()
       // const uploadedImages = await Promise.all(
@@ -30,47 +30,47 @@ const ProductController = {
       //     return (await result).Location
       //   })
       // )
-      const imageArray = [images]
+     
       const newProduct = await Product.create(
         name,
         price,
         size,
-        imageArray,
+        images,
         description,
         stockQuantity,
         featured,
         newArrivals,
         category
-      )
-      res.status(201).json({ ...newProduct, category })
+      );
+      res.status(201).json({ ...newProduct, category });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   async getAllProducts(_: Request, res: Response, next: NextFunction) {
     try {
-      const products = await Product.getAllProducts()
-      res.json(products)
+      const products = await Product.getAllProducts();
+      res.json(products);
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   // get unique product
   async getProductById(req: Request, res: Response, next: NextFunction) {
     try {
-      const { id } = req.params
-      const product = await Product.getProductsById(parseInt(id))
-      res.json(product)
+      const { id } = req.params;
+      const product = await Product.getProductsById(parseInt(id));
+      res.json(product);
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
 
   async updateProduct(req: Request, res: Response, next: NextFunction) {
     try {
-      const { productId } = req.params
+      const { productId } = req.params;
       if (!productId) {
-        throw new Error('Missing the correct endpoint')
+        throw new Error("Missing the correct endpoint");
       }
       const {
         name,
@@ -82,7 +82,7 @@ const ProductController = {
         featured,
         newArrivals,
         category,
-      } = req.body
+      } = req.body;
       const updatedProduct = await Product.updateProduct(
         parseInt(productId),
         name,
@@ -94,23 +94,39 @@ const ProductController = {
         featured,
         newArrivals,
         category
-      )
-      return res.status(201).json({ ...updatedProduct, category })
+      );
+      return res.status(201).json({ ...updatedProduct, category });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   async deleteProduct(req: Request, res: Response, next: NextFunction) {
     try {
-      const { productId } = req.params
+      const { productId } = req.params;
       if (productId === null) {
-        throw new Error('Product not found')
+        throw new Error("Product not found");
       }
-      const product = await Product.deleteProduct(parseInt(productId))
-      res.json({ message: ' Product deleted successfully', data: product })
+      const product = await Product.deleteProduct(parseInt(productId));
+      res.json({ message: " Product deleted successfully", data: product });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
-}
-export default ProductController
+  async getFeaturedProducts(req: Request, res: Response, next: NextFunction) {
+    try {
+      const featuredProducts = await Product.findFeatured();
+      res.status(200).json(featuredProducts);
+    } catch (error) {
+      next(error);
+    }
+  },
+  async getNewArrivals(req: Request, res: Response, next: NextFunction) {
+    try {
+      const newArrivals = await Product.findNewArrivals();
+      res.status(200).json(newArrivals);
+    } catch (error) {
+      next(error);
+    }
+  },
+};
+export default ProductController;
