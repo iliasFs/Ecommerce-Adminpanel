b12316d9d223edcab5de3a80b64a22164293afa6
AgnesGrace <agnesaugustine42@gmail.com>
2023-05-16 18:14:30 +0200

    feat:[client/detailed_product]:added the detailed_product page with the slider functionality

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,12 +1,8 @@
-import NavBar from './components/layouts/NavBar'
-import Footer from './components/layouts/Footer'
 import ProductDetails from './components/pages/ProductDetails'
 function App() {
   return (
     <>
-      <NavBar />
       <ProductDetails />
-      <Footer />
     </>
   )
 }
--- apps/client/src/components/pages/ProductDetails.tsx
+++ apps/client/src/components/pages/ProductDetails.tsx
@@ -5,8 +5,9 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 import { Product } from '../../types/customTypes'
-const ProductDetails: React.FC<Product> = () => {
+const ProductDetails: React.FC = () => {
   const [product, setProduct] = useState([])
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0)
   const fetchProducts = async () => {
     const res = await axios.get('http://localhost:8002/product/5')
     setProduct(res.data)
@@ -15,49 +16,81 @@ const ProductDetails: React.FC<Product> = () => {
     fetchProducts()
   }, [])
   console.log(product)
+  const handleSlideChange = (index: number) => {
+    setSelectedImageIndex(index)
+  }
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+    beforeChange: (current: number, next: number) => handleSlideChange(next),
   }
 
   return (
-    <main className="h-full w-full p-8">
+    <main className="h-full w-full p-8 bg-blue-950 text-white">
       {Object.keys(product).length !== 0 && (
         <>
-          <section className="w-full h-[60%] flex  justify-between mb-4">
-            <div>
-              <img src={product.images[0]} className="rounded" />
+          <section className="w-full h-[60%] flex flex-col md:flex-row justify-between  mb-4">
+            <div className="">
+              <img
+                src={product.images[selectedImageIndex]}
+                alt={`Product Image ${selectedImageIndex + 1}`}
+                className="rounded"
+              />
             </div>
             <aside>
               <div className="flex justify-between">
-                <h1 className="text-4xl">{product.name}</h1>
+                <h1 className="text-4xl uppercase">{product.name}</h1>
                 <p>€{product.price}</p>
               </div>
               <p className="text-2xl mb-4">{product.description}</p>
-              <div className="flex flex-col gap-4 border p-8 mb-4 border-gray-400">
+              <div className="flex flex-col gap-4 border p-8 mb-4 border-gray-400 ">
                 <div className="flex w-full justify-around">
                   <div className="w-16 h-16 border border-gray-500 rounded-full flex justify-center items-center">
-                    <p className="text-white text-lg">S</p>
+                    <p className=" text-lg">S</p>
                   </div>
                   <div className="w-16 h-16 border border-gray-500 rounded-full flex justify-center items-center">
-                    <p className="text-white text-lg">XS</p>
+                    <p className=" text-lg">XS</p>
                   </div>
                   <div className="w-16 h-16 border border-gray-500 rounded-full flex justify-center items-center">
-                    <p className="text-white text-lg">M</p>
+                    <p className=" text-lg">M</p>
                   </div>
                 </div>
                 <div className="flex w-full justify-around gap-4 mb-4">
                   <div className="w-16 h-16 border border-gray-500 rounded-full flex justify-center items-center">
-                    <p className="text-white text-lg">L</p>
+                    <p className=" text-lg">L</p>
                   </div>
                   <div className="w-16 h-16 border border-gray-500 rounded-full flex justify-center items-center">
-                    <p className="text-white text-lg">XL</p>
+                    <p className=" text-lg">XL</p>
                   </div>
                   <div className="w-16 h-16 border border-gray-500 rounded-full flex justify-center items-center">
-                    <p className="text-white text-lg">XXL</p>
+                    <p className=" text-lg">XXL</p>
                   </div>
                 </div>
               </div>
@@ -65,13 +98,7 @@ const ProductDetails: React.FC<Product> = () => {
             </aside>
           </section>
 
-          <Slider
-            infinite
-            slidesToShow={2}
-            slidesToScroll={1}
-            speed={500}
-            className="slider"
-          >
+          <Slider {...settings}>
             <div>
               <img src={product.images[0]} alt="Product Image 1" />
             </div>
