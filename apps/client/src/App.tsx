6b0b92fc5654a9bb3ab8ae304d3edb3df9103bf1
AgnesGrace <agnesaugustine42@gmail.com>
2023-05-16 19:00:42 +0200

    fix:reolved conflicts

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,10 +1,9 @@
-
-import CategoryPage from "./components/CategoryPage";
-import Layout from "./components/Layout";
+import CategoryPage from './components/CategoryPage'
+import Layout from './components/Layout'
 // import ProductDetails from './components/pages/ProductDetails'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import { CartProvider } from "./contexts/CartContext";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import { CartProvider } from './contexts/CartContext'
 function App() {
   return (
     <CartProvider>
@@ -12,15 +11,13 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
-            <Route path="/:categoryName" element= {<CategoryPage />}/>
+            <Route path="/:categoryName" element={<CategoryPage />} />
             {/* <Route path = '/:productId' element={ <ProductDetails />}/> */}
           </Route>
         </Routes>
       </BrowserRouter>
     </CartProvider>
-
   )
 }
-
 
 export default App
--- apps/client/src/components/layouts/Footer.tsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer footer-center bg-black p-4 h-[250px] bottom-0">
-      <p>Copyright &copy; 2023 - All right reserved</p>
-    </footer>
-  )
-}
-
-export default Footer
