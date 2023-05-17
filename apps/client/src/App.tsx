95f476f3b1f474da2c3a9b82c3769080041fc204
Max Puig Llobregat <max@Maxs-Mac-mini.local>
2023-05-17 12:12:20 +0200

    working on checkout

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,9 +1,11 @@
-import CategoryPage from './components/CategoryPage'
-import Layout from './components/Layout'
-import ProductDetails from './pages/ProductDetails'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import { CartProvider } from './contexts/CartContext'
+import CategoryPage from "./components/CategoryPage";
+import Layout from "./components/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import { CartProvider } from "./contexts/CartContext";
+import Checkout from "./components/Checkout";
+import ProductDetails from "./pages/ProductDetails";
+
 function App() {
   return (
     <CartProvider>
@@ -11,13 +13,17 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
+
             <Route path="/product/:productId" element={<ProductDetails />} />
+
             <Route path="/category/:categoryName" element={<CategoryPage />} />
+            <Route path="/:productId" element={<ProductDetails />} />
+            <Route path="/checkout" element={<Checkout />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </CartProvider>
-  )
+  );
 }
 
-export default App
+export default App;
