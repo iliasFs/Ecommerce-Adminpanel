31620e63048cb36c676daab179b1a327d879d16f
iliasFs <110726288+iliasFs@users.noreply.github.com>
2023-05-27 13:10:18 +0300

    fix(client):made the hero responsive

--- apps/client/src/components/HeroSection.tsx
+++ apps/client/src/components/HeroSection.tsx
@@ -2,15 +2,15 @@ const HeroSection = () => {
   return (
     <div className="flex section__margin items-center w-full h-[calc(100vh-168px)] ">
       <div className="relative w-full bg-[#1D3557] h-[73%] overflow-hidden ">
-        <div className="hidden sm:flex main-letters-1 absolute h-[294px] leading-[293.5px] tracking-[-30px] top-[-64px] left-[490px] font-extrabold">
+        <div className="hidden lg:flex main-letters-1 absolute h-[294px] leading-[293.5px] tracking-[-30px] top-[-64px] left-[490px] font-extrabold">
           <h1 className="whitespace-nowrap text-[450px] border-text ">
             Tees to
           </h1>
         </div>
-        <div className="hidden sm:flex main-letters-2 absolute leading-[293.5px] tracking-[-30px] top-[275px] left-[578px] font-extrabold">
+        <div className="hidden lg:flex main-letters-2 absolute leading-[293.5px] tracking-[-30px] top-[275px] left-[578px] font-extrabold">
           <h1 className="text-[450px] border-text ">bliss</h1>
         </div>
-        <div className="absolute hidden sm:flex top-[10px] w-[291px] h-[120px] left-[620px] ">
+        <div className="absolute third-letters hidden sm:flex top-[10px] w-[291px] h-[120px] left-[620px] ">
           <h1 className="text-[90px] font-extrabold tracking-tight text-white z-20 ">
             Tees <br /> to <br /> bliss
           </h1>
